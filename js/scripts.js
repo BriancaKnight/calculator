579dcff6d67794fe263da87d87ffb17b0b931787
Brianca Knight <briancaknight@gmail.com>
2023-10-16 16:00:26 -0700

    Change alert to return user inputted numbers and results, ammend formatting

--- js/scripts.js
+++ js/scripts.js
@@ -3,31 +3,23 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
-function subtract(number1, number2){
+function subtract(number1, number2) {
   return number1 - number2
 }
 
-function multiply(number1, number2){
+function multiply(number1, number2) {
   return number1 * number2
 }
 
-function divide(number1, number2){
+function divide(number1, number2) {
   return number1 / number2;
 }
 
 // user interface logic 
-const number1 = parseInt (prompt("Enter a number:"));
-const number2 = parseInt (prompt("Enter another number:"));
+const number1 = parseInt(prompt("Enter a number:"));
+const number2 = parseInt(prompt("Enter another number:"));
 
-// window.alert("Your numbers add up to " + add(number1, number2));
-
-// window.alert("Your numbers subtract to " + subtract(number1, number2));
-
-// window.alert("Your numbers multiply to " + multiply(number1, number2));
-
-// window.alert("Your numbers divide to " + divide(number1, number2));
-
-window.alert("The addition of your numbers is " + add(number1, number2) + "." +
-" The subtraction of your numbers is " + subtract(number1, number2) + "." +
-" The multiplication of your numbers is " + multiply(number1, number2) + "." +
-" The division of your numbers is " + divide(number1, number2) + ".")
+window.alert(number1 + "+" + number2 + "=" + add(number1, number2) + ", " +
+  number1 + "-" + number2 + "=" + subtract(number1, number2) + ", " +
+  number1 + "*" + number2 + "=" + multiply(number1, number2) + ", " +
+  number1 + "/" + number2 + "=" + divide(number1, number2));
