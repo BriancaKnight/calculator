ef1a91d25d8c0e3b3363320bfd10e586c64edc3b
Brianca Knight <briancaknight@gmail.com>
2023-10-16 15:53:42 -0700

    combine 4 alerts into 1 that states the result of each mathematical operation.

--- js/scripts.js
+++ js/scripts.js
@@ -19,5 +19,15 @@ function divide(number1, number2){
 const number1 = parseInt (prompt("Enter a number:"));
 const number2 = parseInt (prompt("Enter another number:"));
 
-window.alert(add(number1, number2));
+// window.alert("Your numbers add up to " + add(number1, number2));
 
+// window.alert("Your numbers subtract to " + subtract(number1, number2));
+
+// window.alert("Your numbers multiply to " + multiply(number1, number2));
+
+// window.alert("Your numbers divide to " + divide(number1, number2));
+
+window.alert("The addition of your numbers is " + add(number1, number2) + "." +
+" The subtraction of your numbers is " + subtract(number1, number2) + "." +
+" The multiplication of your numbers is " + multiply(number1, number2) + "." +
+" The division of your numbers is " + divide(number1, number2) + ".")
